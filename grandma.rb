f0eec428d18cd38b8acd07597e9906eb2aa37c27
Erin Green <erin.y.green@gmail.com>
2018-09-18 13:10:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(string)
   if string.match(/[A-Z]/) == true 
     return "NO, NOT SINCE 1938!"
     elsif string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   else return "HUH?! SPEAK UP, SONNY!"
 end 
 end 
