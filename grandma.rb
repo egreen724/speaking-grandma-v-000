6507aeeea8114ff94f14ddcb59041236967fd720
Erin Green <erin.y.green@gmail.com>
2018-09-18 13:07:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(string)
-  if string.upcase == true 
+  if string.match(/[A-Z]/) == true 
     
 
 # Whatever you say to grandma, she should respond with
