7cc9a0191bd933ebdc7dd35f605ae2542b6d8462
Erin Green <erin.y.green@gmail.com>
2018-09-18 13:20:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!" 
    return "I LOVE YOU TOO PUMPKIN!"
-    elsif string.upcase == false
+    elsif string != string.upcase
      return "HUH?! SPEAK UP, SONNY!"   
   else 
     return "NO, NOT SINCE 1938!"
